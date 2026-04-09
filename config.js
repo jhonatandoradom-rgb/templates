335f9a2f7ee0c705a130e62ffb0f9c425c2d59b2
Jhonatan Dorado <jhonatan.dorado.m@gmail.com>
2026-04-09 16:27:27 -0500

    refactor

---
+++ config.js
@@ -0,0 +1,21 @@
+const CONFIG = {
+
+businessName: "Mi Negocio",
+phoneText: "📞 (57) 300 123 4567",
+
+phone: "573001234567",
+
+address: "📍 Melgar, Tolima",
+
+hours: "🕒  Lunes a Domingo 5pm - 11pm",
+
+instagram: "https://www.instagram.com/jsdorado17/",
+
+facebook: "https://web.facebook.com/profile.php?id=100088269885098",
+
+
+type: "restaurante" // cambia esto
+
+ // cambia esto
+
+}
---
+++ data.js
@@ -0,0 +1,46 @@
+const DATA = {
+
+taller: {
+title: "Servicio técnico confiable",
+subtitle: "Diagnóstico rápido y atención inmediata",
+services: [
+    {title: "Cambio de aceite",detail: "Servicio garantizado"}, 
+    {title: "Diagnóstico",detail: "Técnicos certificados"}, 
+    {title: "Reparación",detail: "Atención personalizada"}],
+gallery: [
+"https://images.unsplash.com/photo-1486006920555-c77dcf18193c",
+"https://images.unsplash.com/photo-1503376780353-7e6692767b70",
+"https://images.unsplash.com/photo-1515923256482-1c04580b477c"
+]
+},
+
+peluqueria: {
+title: "Tu estilo empieza aquí",
+banner: "assets/images/hero.webp",
+subtitle: "Cortes modernos y coloración profesional",
+services: ["Corte", "Color", "Peinados"],
+gallery: [
+"https://images.unsplash.com/photo-1521590832167-7bcbfaa6381f",
+"https://images.unsplash.com/photo-1562322140-8baeececf3df",
+"https://images.unsplash.com/photo-1519699047748-de8e457a634e"
+]
+},
+
+restaurante: {
+title: "Sabores que enamoran",
+banner: "assets/images/hero.jpg",
+subtitle: "Comida deliciosa para todos",
+titleGallery: "Menu destacado",
+buttonDownloadLabel: "Descargar menú",
+services: [
+    {title: "Platos", detail: "Especialidades locales"}, 
+    {title: "Domicilios", detail: "Entrega rápida y segura"}, 
+    {title: "Reservas", detail: "Mesas disponibles"}],
+gallery: [
+"https://images.unsplash.com/photo-1504674900247-0877df9cc836",
+"https://images.unsplash.com/photo-1546069901-ba9599a7e63c",
+"https://images.unsplash.com/photo-1551218808-94e220e084d2"
+]
+}
+
+}
